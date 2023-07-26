9608ef49bc24aeec8d9c2a1bf2e982b5830c26dc
askerbekov <askerbekov.mederbek@gmail.com>
2023-07-26 13:39:22 +0600

    added symbols currency

--- script.js
+++ script.js
@@ -4,14 +4,21 @@ const input = document.querySelector('#input')
 const convertBtn = document.querySelector('#convertBtn')
 const res = document.querySelector('#res')
 
+let curSymbol = {
+    EUR:'€',
+    USD:'$',
+    GBP:'£',
+    KGS:'лв',
+}
 const handleConvert = () => {
+    // console.log(currencyTo.value
     if (currencyTo.value === currency.value){
         alert('error')
     } else {
         fetch(`https://api.exchangerate.host/latest?base=${currencyTo.value}&symbols=${currency.value}&amount=${input.value}&places=2`)
             .then(response => response.json())
             .then(data => {
-                res.innerHTML = `${input.value} ${currencyTo.value} = ${Object.values(data.rates)} ${currency.value} `
+                res.innerHTML = `${input.value} ${currencyTo.value = curSymbol[currencyTo.value]} = ${Object.values(data.rates)} ${currency.value = curSymbol[currency.value]} `
             })
     }
 
